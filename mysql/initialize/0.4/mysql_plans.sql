790e83c280da3c4909a9d6dd8c4174af78ed9dfc
Stefan Pejcic <stefan@pejcic.rs>
2024-11-01 17:38:32 +0100

    Update mysql_plans.sql

--- mysql/initialize/0.4/mysql_plans.sql
+++ mysql/initialize/0.4/mysql_plans.sql
@@ -122,11 +122,8 @@ CREATE TABLE `active_sessions` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `expires_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `active_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 --
@@ -164,6 +161,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+
+--
+-- AUTO_INCREMENT for table `active_sessions`
+--
+ALTER TABLE `active_sessions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 --
 -- AUTO_INCREMENT for table `domains`
 --
@@ -191,6 +195,14 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+
+--
+-- Constraints for table `active_sessions`
+--
+ALTER TABLE `active_sessions`
+  ADD CONSTRAINT `active_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
 
 --
 -- Constraints for table `domains`
